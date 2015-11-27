55718e4cc706341058d8bd2192598f9fa6ca8e22
Lorenz Schori <lo@znerol.ch>
2015-11-27 19:02:53 +0100

    Add specifiers and bump version

--- setup.py
+++ setup.py
@@ -6,11 +6,25 @@ from setuptools import setup
 
 setup(
     name='idlk',
-    version='0.0.1',
+    version='0.0.9',
     description='idlk lock filename generator',
     author='Lorenz Schori',
     author_email='lo@znerol.ch',
+    url='https://github.com/znerol/py-idlk',
     packages=['idlk'],
     test_suite="idlk.test",
-    zip_safe=True
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Graphics'
+    ]
 )
